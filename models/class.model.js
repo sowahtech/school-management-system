00745fd2d8c60e95773941ad623c2f72fc018dbb
sowahtech <sowahtech@gmail.com>
2024-12-21 21:31:15 +0000

    class model created

---
+++ models/class.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const classSchema = mongoose.Schema({
+  school: { type: mongoose.Schema.ObjectId, ref: "School" },
+  class_test: { type: String, required: true },
+  class_num: { type: Number, required: true },
+  attendee: { type: mongoose.Schema.ObjectId, ref: "Teacher" },
+  createAt: { type: Date, default: new Date() },
+});
+
+module.exports = mongoose.model("Class", classSchema);
