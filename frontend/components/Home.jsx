98e64f9bc2517b32186a3103aedbf3f0497e56e1
Justice Sowah <sowahtech@gmail.com>
2025-07-22 23:52:16 +0000

    imported styled components for home.jsx from styles folder

---
+++ frontend/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Navbar, 
+        NavLink, 
+        ButtonsContainer, 
+        LogInButton, 
+        GuestButton, 
+        HomeContainer, 
+        SchoolInfo, 
+        SchoolImage, 
+        Title, 
+        LoremTextContainer, 
+        AdminRegisterLink
+    } from '../styles/styles'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
