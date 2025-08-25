04ad59464491086c76bf75bbb9968480cc7a6b21
Justice Sowah <sowahtech@gmail.com>
2025-08-25 17:16:37 +0000

    imported neccessary styled components into dashboard component

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -3,6 +3,18 @@ import EventCalendar from "./EventCalendar";
 import Announcement from "./Announcement";
 import Performance from "./Performance";
 import axios from "axios";
+import {
+  AdminDashboardContainer,
+  Content,
+  TopContent,
+  BottomContent,
+  Section,
+  SectionTitle,
+  CardContainer,
+  Card,
+  CardContent,
+  CardTitle,
+} from "../../styles/DashboardStyles";
 const AdminDashboard = () => {
   return <>Admin Dashboard</>;
 };
