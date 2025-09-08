9e83c3a0511180087a7dc41a682be6505eab9973
Justice Sowah <sowahtech@gmail.com>
2025-09-08 20:56:18 +0000

    coded assignment.jsx component

--- frontend/src/pages/Admin/Assignments.jsx
+++ frontend/src/pages/Admin/Assignments.jsx
@@ -1,7 +1,50 @@
 import { useState, useEffect } from "react";
+import Sidebar from "./Sidebar";
+import axios from "axios";
+
+import {
+  AssignmentsContainer,
+  Content,
+  AssignmentsContent,
+  AssignmentsHeader,
+  AssignmentList,
+  AssignmentItem,
+  AddAssignmentForm,
+  AddAssignmentInput,
+  AddAssignmentTextArea,
+  AddAssignmentButton,
+} from "../../styles/AssignmentsStyles";
 
 const Assignments = () => {
-  return <div>Assignments</div>;
+  return (
+    <AssignmentsContainer>
+      <Sidebar />
+      <Content>
+        <AssignmentsContent>
+          <AssignmentsHeader>Assignments</AssignmentsHeader>
+          <AddAssignmentForm>
+            <AddAssignmentInput
+              type="text"
+              placeholder="Enter Assignment Title"
+            />
+            <AddAssignmentTextArea placeholder="Enter assignment description" />
+            <AddAssignmentInput
+              type="text"
+              placeholder="Enter assignment Grade"
+            />
+            <AddAssignmentInput
+              type="text"
+              placeholder="Enter assignment Deadline"
+            />
+            <AddAssignmentButton type="submit">
+              Add Assignment
+            </AddAssignmentButton>
+          </AddAssignmentForm>
+          <AssignmentList></AssignmentList>
+        </AssignmentsContent>
+      </Content>
+    </AssignmentsContainer>
+  );
 };
 
 export default Assignments;
