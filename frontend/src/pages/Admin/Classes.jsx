25c42a64c4b9fba712697a57a55f7bf2bac05400
Justice Sowah <sowahtech@gmail.com>
2025-09-08 20:02:29 +0000

    Coded classes.jsx component

--- frontend/src/pages/Admin/Classes.jsx
+++ frontend/src/pages/Admin/Classes.jsx
@@ -1,7 +1,34 @@
 import { useState, useEffect } from "react";
+import Sidebar from "./Sidebar";
+import axios from "axios";
+import {
+  ClassContainer,
+  Content,
+  ClassesContent,
+  ClassHeader,
+  ClassList,
+  ClassItem,
+  AddClassButton,
+  AddClassForm,
+  AddClassInput,
+} from "../../styles/ClassesStyles";
 
 const Classes = () => {
-  return <div>Classes</div>;
+  return (
+    <ClassContainer>
+      <Sidebar />
+      <Content style={{ paddingLeft: 250 }}>
+        <ClassesContent>
+          <ClassHeader>Classes</ClassHeader>
+          <AddClassForm>
+            <AddClassInput type="text" placeholder="Enter class name" />
+            <AddClassButton type="submit">Add Class</AddClassButton>
+          </AddClassForm>
+          <ClassList></ClassList>
+        </ClassesContent>
+      </Content>
+    </ClassContainer>
+  );
 };
 
 export default Classes;
