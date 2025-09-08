dab14d0f0159580fde74e1f814cc5c2082891c3a
Justice Sowah <sowahtech@gmail.com>
2025-09-08 20:36:54 +0000

    coded teacher.jsx component

--- frontend/src/pages/Admin/Teachers.jsx
+++ frontend/src/pages/Admin/Teachers.jsx
@@ -1,7 +1,41 @@
 import { useState, useEffect } from "react";
+import Sidebar from "./Sidebar";
+import axios from "axios";
+
+import {
+  TeachersContainer,
+  Content,
+  TeachersContent,
+  TeachersHeader,
+  TeacherList,
+  TeacherItem,
+  AddTeacherForm,
+  AddTeacherInput,
+  AddTeacherButton,
+} from "../../styles/TeachersStyles";
 
 const Teachers = () => {
-  return <div>Teachers</div>;
+  return (
+    <TeachersContainer>
+      <Sidebar />
+      <Content>
+        <TeachersContent>
+          <TeachersHeader>
+            <AddTeacherForm>
+              <AddTeacherInput type="text" placeholder="Enter Teacher Name" />
+              <AddTeacherInput type="email" placeholder="Enter Teacher Email" />
+              <AddTeacherInput
+                type="text"
+                placeholder="Enter Teacher Subject"
+              />
+              <AddTeacherButton type="submit">Add Teacher</AddTeacherButton>
+            </AddTeacherForm>
+            <TeacherList></TeacherList>
+          </TeachersHeader>
+        </TeachersContent>
+      </Content>
+    </TeachersContainer>
+  );
 };
 
 export default Teachers;
