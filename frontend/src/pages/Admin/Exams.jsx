aeb4ade6c0f9a37f45de975e664280cb2779fc85
Justice Sowah <sowahtech@gmail.com>
2025-09-09 23:54:56 +0000

    coded exams.jsx component

--- frontend/src/pages/Admin/Exams.jsx
+++ frontend/src/pages/Admin/Exams.jsx
@@ -1,7 +1,50 @@
 import { useState, useEffect } from "react";
+import Sidebar from "./Sidebar";
+import axios from "axios";
+
+import {
+  ExamContainer,
+  SidebarContainer,
+  Content,
+  ExamHeader,
+  ExamForm,
+  FormLabel,
+  FormInput,
+  AddButton,
+} from "../../styles/ExamStyles";
 
 const Exams = () => {
-  return <div>Exams</div>;
+  const calculateTotalMarks = () => {
+    let total = 0;
+    for (i = 0, i < examData.length; i++; ) {
+      total = examData[i].marks;
+    }
+    return total;
+  };
+  return (
+    <ExamContainer>
+      <SidebarContainer>
+        <Sidebar />
+      </SidebarContainer>
+      <Content>
+        <ExamHeader>Exam Details</ExamHeader>
+        <ExamForm>
+          <FormLabel>Name: </FormLabel>
+          <FormInput type="text" required />
+          <FormLabel>Registration Number: </FormLabel>
+          <FormInput type="text" required />
+          <FormLabel>Class: </FormLabel>
+          <FormInput type="text" required />
+          <FormLabel>Marks: </FormLabel>
+          <FormInput type="number" required />
+          <AddButton type="submit">Add Exam</AddButton>
+        </ExamForm>
+        <h2>Total Marks: {calculateTotalMarks} </h2>
+        <h3>Exam Details: </h3>
+        <ul></ul>
+      </Content>
+    </ExamContainer>
+  );
 };
 
 export default Exams;
