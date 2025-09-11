5025870fe5a8e551064b8bf78f349527500f8d67
Justice Sowah <sowahtech@gmail.com>
2025-09-11 22:51:05 +0000

    coded announcement.jsx component

--- frontend/src/pages/Admin/Announcement.jsx
+++ frontend/src/pages/Admin/Announcement.jsx
@@ -1,7 +1,40 @@
 import { useState, useEffect } from "react";
+import Sidebar from "./Sidebar";
+import axios from "axios";
+
+import {
+  AnnouncementContainer,
+  Content,
+  Title,
+  AnnouncementForm,
+  FormGroup,
+  Label,
+  TextArea,
+  Button,
+  AnnouncementList,
+  AnnouncementItem,
+  AnnouncementContent,
+} from "../../styles/AnnouncementStyles";
 
 const Announcement = () => {
-  return <>Announcements</>;
+  return (
+    <AnnouncementContainer>
+      <Sidebar />
+      <Content>
+        <Title>Announcement</Title>
+        <AnnouncementForm>
+          <FormGroup>
+            <Label htmlFor="announcement">Announcement: </Label>
+            <TextArea id="announcement" required rows={4} cols={50} />
+          </FormGroup>
+          <Button type="submit">Send Announcement</Button>
+        </AnnouncementForm>
+        {/* To display announcements */}
+        <h2>Announcement</h2>
+        <AnnouncementList></AnnouncementList>
+      </Content>
+    </AnnouncementContainer>
+  );
 };
 
 export default Announcement;
