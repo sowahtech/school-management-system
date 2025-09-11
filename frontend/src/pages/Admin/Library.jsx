ddff96d13cd0a952d025b7cb0008320838b77e10
Justice Sowah <sowahtech@gmail.com>
2025-09-11 22:29:13 +0000

    coded library.jsx component

--- frontend/src/pages/Admin/Library.jsx
+++ frontend/src/pages/Admin/Library.jsx
@@ -1,7 +1,46 @@
 import { useState, useEffect } from "react";
+import Sidebar from "./Sidebar";
+import axios from "axios";
+
+import {
+  LibraryContainer,
+  Content,
+  Title,
+  AddBookForm,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  BookList,
+  BookItem,
+  BookTitle,
+  BookAuthor,
+  ActionButton,
+} from "../../styles/LibraryStyles";
 
 const Library = () => {
-  return <div>Library</div>;
+  return (
+    <LibraryContainer style={{ paddingLeft: 250 }}>
+      <Sidebar />
+      <Content>
+        <Title>Library Management</Title>
+        <AddBookForm>
+          <h2>Add New Book</h2>
+          <FormGroup>
+            <Label htmlFor="title">Title</Label>
+            <input type="text" id="title" />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="author">Author</Label>
+            <input type="text" id="author" />
+          </FormGroup>
+          <Button type="submit">Add Book</Button>
+        </AddBookForm>
+        <h2>Books</h2>
+        <BookList></BookList>
+      </Content>
+    </LibraryContainer>
+  );
 };
 
 export default Library;
