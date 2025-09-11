a087e5095150a56a84ff16b01bed8c0ef33c1a4a
Justice Sowah <sowahtech@gmail.com>
2025-09-11 22:08:55 +0000

    coded attendance.jsx component

--- frontend/src/pages/Admin/Attendance.jsx
+++ frontend/src/pages/Admin/Attendance.jsx
@@ -1,7 +1,49 @@
 import { useState, useEffect } from "react";
+import Sidebar from "./Sidebar";
+import axios from "axios";
+
+import {
+  AttendanceContainer,
+  Content,
+  AttendanceContent,
+  AttendanceHeader,
+  AttendanceList,
+  AttendanceItem,
+  StudentName,
+  CheckboxLabel,
+  Divider,
+  SubmitButton,
+} from "../../styles/AttendanceStyles";
 
 const Attendance = () => {
-  return <div>Attendance</div>;
+  return (
+    <AttendanceContainer>
+      <Sidebar />
+      <Content>
+        <AttendanceContent>
+          <AttendanceHeader>Attendance</AttendanceHeader>
+          <AttendanceList>
+            <AttendanceItem>
+              <StudentName></StudentName>
+              <CheckboxLabel>
+                <input type="checkbox" />
+                Present
+              </CheckboxLabel>
+              <CheckboxLabel>
+                <input type="checkbox" />
+                Absent
+              </CheckboxLabel>
+              <CheckboxLabel>
+                <input type="checkbox" />
+                Absent with apology
+              </CheckboxLabel>
+            </AttendanceItem>
+          </AttendanceList>
+          <SubmitButton>Submit</SubmitButton>
+        </AttendanceContent>
+      </Content>
+    </AttendanceContainer>
+  );
 };
 
 export default Attendance;
