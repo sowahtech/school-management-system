1cf74d258ea5a59aea1e9d1bd183583fff8b0095
Justice Sowah <sowahtech@gmail.com>
2025-09-14 21:27:55 +0000

    coded performance.jsx component

--- frontend/src/pages/Students/Performance.jsx
+++ frontend/src/pages/Students/Performance.jsx
@@ -1,10 +1,68 @@
 import { useState, useEffect } from "react";
 import Sidebar from "./Sidebar";
 import axios from "axios";
+import { Line } from "react-chartjs-2";
 
-import {} from "../../styles/DashboardStyles";
+import {
+  PerformanceContainer,
+  SidebarContainer,
+  Content,
+  PerformanceHeader,
+  PerformanceInfo,
+  PerformanceGraphContainer,
+  TotalMarks,
+} from "../../styles/PerformanceStyles";
 const PerformanceSection = () => {
-  return <>hello</>;
+  // static data
+  const performanceData = {
+    months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    marks: [90, 85, 87, 92, 85, 89],
+    totalMarks: 520,
+  };
+
+  // lets make line chart data here
+  const lineChartData = {
+    labels: performanceData.months,
+    datasets: [
+      {
+        label: "Performance Trends",
+        fill: "false",
+        lineTension: 0.1,
+        backgroundColor: "#007bff",
+        borderColor: "#007bcc",
+        data: performanceData.marks,
+      },
+    ],
+  };
+  return (
+    <PerformanceContainer>
+      <SidebarContainer>
+        <Sidebar />
+      </SidebarContainer>
+      <Content>
+        <PerformanceHeader>Performance</PerformanceHeader>
+        <PerformanceInfo>
+          <PerformanceGraphContainer>
+            <Line
+              data={lineChartData}
+              options={{
+                scales: {
+                  yAxes: [
+                    {
+                      tickes: {
+                        beginAtZero: true,
+                      },
+                    },
+                  ],
+                },
+              }}
+            />
+          </PerformanceGraphContainer>
+          <TotalMarks>Total Marks: {performanceData.totalMarks}</TotalMarks>
+        </PerformanceInfo>
+      </Content>
+    </PerformanceContainer>
+  );
 };
 
 export default PerformanceSection;
