fffcba3fb99eef99c48b001a66a773d6f4d4a0ec
Justice Sowah <sowahtech@gmail.com>
2025-09-14 22:57:12 +0000

    coded student profile section

--- frontend/src/pages/Students/Profile.jsx
+++ frontend/src/pages/Students/Profile.jsx
@@ -2,9 +2,58 @@ import { useState, useEffect } from "react";
 import Sidebar from "./Sidebar";
 import axios from "axios";
 
-import {} from "../../styles/DashboardStyles";
+import {
+  ProfileContainer,
+  SidebarContainer,
+  Content,
+  ProfileHeader,
+  ProfileInfo,
+  ProfileDetail,
+  Label,
+  Value,
+} from "../../styles/SettingsProfileStyles";
+
 const ProfileSection = () => {
-  return <>hello</>;
+  const studentProfile = {
+    name: "Johnny Doe",
+    age: 17,
+    grade: "11th",
+    school: "Does high school",
+    email: "johnny@doe.com",
+  };
+
+  return (
+    <ProfileContainer>
+      <SidebarContainer>
+        <Sidebar />
+      </SidebarContainer>
+      <Content>
+        <ProfileHeader>Profile</ProfileHeader>
+        <ProfileInfo>
+          <ProfileDetail>
+            <Label>Name :</Label>
+            <Value>{studentProfile.name}</Value>
+          </ProfileDetail>
+          <ProfileDetail>
+            <Label>Age :</Label>
+            <Value>{studentProfile.age}</Value>
+          </ProfileDetail>
+          <ProfileDetail>
+            <Label>Grade :</Label>
+            <Value>{studentProfile.grade}</Value>
+          </ProfileDetail>
+          <ProfileDetail>
+            <Label>School :</Label>
+            <Value>{studentProfile.school}</Value>
+          </ProfileDetail>
+          <ProfileDetail>
+            <Label>Email :</Label>
+            <Value>{studentProfile.email}</Value>
+          </ProfileDetail>
+        </ProfileInfo>
+      </Content>
+    </ProfileContainer>
+  );
 };
 
 export default ProfileSection;
