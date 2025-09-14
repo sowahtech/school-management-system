4f42c6d045b3eb02aca9632d4ed86e8849b5e4a6
Justice Sowah <sowahtech@gmail.com>
2025-09-14 22:27:01 +0000

    coded students announcement section

--- frontend/src/pages/Students/Announcements.jsx
+++ frontend/src/pages/Students/Announcements.jsx
@@ -2,9 +2,30 @@ import { useState, useEffect } from "react";
 import Sidebar from "./Sidebar";
 import axios from "axios";
 
-import {} from "../../styles/DashboardStyles";
+import {
+  AnnouncementContainer,
+  SidebarContainer,
+  Content,
+  AnnouncementHeader,
+  AnnouncementList,
+  AnnouncementItem,
+  AnnouncementTitle,
+  AnnouncementContent,
+} from "../../styles/AnnouncementStyles";
+
 const AnnouncementSection = () => {
-  return <>hello</>;
+  return (
+    <AnnouncementContainer>
+      <SidebarContainer>
+        <Sidebar />
+      </SidebarContainer>
+      <Content>
+        <AnnouncementHeader>Announcements</AnnouncementHeader>
+        {/* here we need to map through the announcements coming from the database */}
+        <AnnouncementList></AnnouncementList>
+      </Content>
+    </AnnouncementContainer>
+  );
 };
 
 export default AnnouncementSection;
