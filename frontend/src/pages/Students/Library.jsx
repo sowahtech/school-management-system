de5ec855c53e270924d184f4557328cb08e74475
Justice Sowah <sowahtech@gmail.com>
2025-09-14 22:17:26 +0000

    coded the student library section

--- frontend/src/pages/Students/Library.jsx
+++ frontend/src/pages/Students/Library.jsx
@@ -2,9 +2,30 @@ import { useState, useEffect } from "react";
 import Sidebar from "./Sidebar";
 import axios from "axios";
 
-import {} from "../../styles/DashboardStyles";
+import {
+  LibraryContainer,
+  SidebarContainer,
+  Content,
+  LibraryHeader,
+  BookList,
+  BookItem,
+  BookTitle,
+  BorrowButton,
+} from "../../styles/LibraryStyles";
+
 const LibrarySection = () => {
-  return <>hello</>;
+  return (
+    <LibraryContainer>
+      <SidebarContainer>
+        <Sidebar />
+      </SidebarContainer>
+      <Content>
+        <LibraryHeader>Library</LibraryHeader>
+        {/* here we will map through the books coming from the database */}
+        <BookList></BookList>
+      </Content>
+    </LibraryContainer>
+  );
 };
 
 export default LibrarySection;
