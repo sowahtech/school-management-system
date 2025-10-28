a7b652d6a8d605fe277d957bdcc7ffc3f548929b
Justice Sowah <sowahtech@gmail.com>
2025-10-28 09:59:04 +0000

    used student router, dbConnection, express.json(), created student route in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
 import express from "express"
 import {config} from "dotenv"
+import cors from "cors"
+import studentRouter from "./routers/studentRouter.js"
+import { dbConnection } from "./database/dbConnecion.js"
 
 const app = express()
 config({path: './config/config.env'})
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.use('/api/vt/students', studentRouter)
+
+dbConnection()
 
 export default app;
