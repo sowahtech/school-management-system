200379474bb655141a342a458e5e49dc21a039b2
Justice Sowah <sowahtech@gmail.com>
2025-10-28 09:57:20 +0000

    handled validation errors in studentController.js

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -1,11 +1,13 @@
-import { Student } from "../models/studentSchema";
+import { Student } from "../models/studentSchema.js";
+import { handleValidationError } from "../middlewares/errorHandler.js";
 
 export const createStudent = async(req, res, next) =>{
+    console.log(req.body)
     const {name, registrationNumber, grade} = req.body
 
     try {
         if(!name || !registrationNumber || !grade){
-            return next("Please fill out the full form", 400)
+            handleValidationError("Please fill out the full form", 400)
         }
         await Student.create({name, registrationNumber, grade})
         res.status(200).json({
