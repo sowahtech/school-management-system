739314f1db3ab97956ecae171da46204431a72f2
Justice Sowah <sowahtech@gmail.com>
2026-03-05 15:50:39 +0000

    placed mongodb url in .env file

--- backend/database/dbConnecion.js
+++ backend/database/dbConnecion.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const dbConnection = () => {
     mongoose.connect(process.env.MONGO_URL, {
-        dbName : "SCHOOL_MANAGEMENT_SYSTEM"
+        dbName : "School_Management_System"
     }).then(
         console.log("connected to database successfully")
     ).catch((error) =>{
