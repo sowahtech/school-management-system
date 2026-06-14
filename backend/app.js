8c4b037e7dbf73001f362503f2053ab7bd6ba04f
Justice Sowah <sowahtech@gmail.com>
2026-06-14 17:05:10 +0000

    configured dotenv at the top of the app.js file

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
-import express from "express"
+
 import dotenv from "dotenv"
+dotenv.config()
+import express from "express"
 import cors from "cors"
 import studentRouter from "./routers/studentRouter.js"
 import eventsRouter from "./routers/eventsRouter.js"
@@ -17,6 +19,7 @@ const app = express()
 // config({path: './config/config.env'})
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+console.log(process.env.FRONTEND_URL)
 app.use(cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"]
