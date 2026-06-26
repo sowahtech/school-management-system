2a007caa75ac39c8a13bf1cd08195e4e2fc31391
Justice Sowah <sowahtech@gmail.com>
2026-06-26 18:49:52 +0000

    asynchronous fetch class function

--- frontend/src/pages/Admin/Classes.jsx
+++ frontend/src/pages/Admin/Classes.jsx
@@ -22,7 +22,7 @@ const Classes = () => {
     fetchClasses();
   }, [])
 
-  const fetchClasses = async() => {
+  const fetchClasses = async () => {
     try {
       const response = await axios.get("http://localhost:4000/api/vt/class/getall")
       if (response.data && Array.isArray(response.data.classes)) {
