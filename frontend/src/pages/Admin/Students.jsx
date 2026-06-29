8b77761d6c0bb84b523be85bba2a89b657cd6c7b
Justice Sowah <sowahtech@gmail.com>
2026-06-29 17:39:20 +0000

    students is an array

--- frontend/src/pages/Admin/Students.jsx
+++ frontend/src/pages/Admin/Students.jsx
@@ -16,10 +16,10 @@ import {
 
 const Students = () => {
 
-  const [newStudent, setNewStudent] = useState({name: '', registrationNumber: '', grade: ''})
+  const [newStudent, setNewStudent] = useState({ name: '', registrationNumber: '', grade: '' })
   const [students, setStudents] = useState([])
 
-    useEffect(() => {
+  useEffect(() => {
     fetchStudents();
   }, [])
 
@@ -38,8 +38,8 @@ const Students = () => {
       try {
         const response = await axios.post('http://localhost:4000/api/vt/students/create', newStudent)
         console.log('Response data: ', response.data) // this will log the response data
-        setStudents(...students, response.data.students)
-        setNewStudent({name: '', registrationNumber: '', grade: ''})
+        setStudents([...students, response.data.students])
+        setNewStudent({ name: '', registrationNumber: '', grade: '' })
         setNewClassName('')
       } catch (error) {
         console.error('Error adding student: ', error)
@@ -54,17 +54,17 @@ const Students = () => {
         <StudentsContent>
           <StudentsHeader>
             <AddStudentForm onSubmit={handleAddStudent}>
-              <AddStudentInput  type="text" placeholder="Enter Student Name" value = {newStudent.name} onChange = {(e) => setNewStudent({...newStudent, name: e.target.value})} />
+              <AddStudentInput type="text" placeholder="Enter Student Name" value={newStudent.name} onChange={(e) => setNewStudent({ ...newStudent, name: e.target.value })} />
               <AddStudentInput
                 type="text"
                 placeholder="Enter Registration Number"
-                value = {newStudent.registrationNumber} onChange = {(e) => setNewStudent({...newStudent, registrationNumber: e.target.value})}
+                value={newStudent.registrationNumber} onChange={(e) => setNewStudent({ ...newStudent, registrationNumber: e.target.value })}
               />
-              <AddStudentInput type="text" placeholder="Enter Grade" value = {newStudent.grade} onChange = {(e) => setNewStudent({...newStudent, grade: e.target.value})}/>
+              <AddStudentInput type="text" placeholder="Enter Grade" value={newStudent.grade} onChange={(e) => setNewStudent({ ...newStudent, grade: e.target.value })} />
               <AddStudentButton type="submit">Add Student</AddStudentButton>
             </AddStudentForm>
             <StudentList>
-              {students.map((student) =>{
+              {students.map((student) => {
                 <StudentItem>{student.name} - {student.registrationNumber} - {student.grade}</StudentItem>
               })}
             </StudentList>
