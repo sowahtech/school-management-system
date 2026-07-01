1117b0391d84d270dc0b43632b636e5df6dce406
Justice Sowah <sowahtech@gmail.com>
2026-07-01 22:58:41 +0000

    connected admin/EventCalendar.jsx to backend

--- frontend/src/pages/Admin/EventCalendar.jsx
+++ frontend/src/pages/Admin/EventCalendar.jsx
@@ -15,6 +15,43 @@ import {
 } from "../../styles/EventCalendarStyles";
 
 const EventCalendar = () => {
+
+  const [events, setEvents] = useState([])
+  const [newEvent, setNewEvent] = useState('')
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    fetchEvents();
+  }, [])
+
+  const fetchEvents = async () => {
+    try {
+      const response = await axios.get("http://localhost:4000/api/vt/events/getall")
+      setEvents(response.data.events || [])
+    } catch (error) {
+      console.error('Error fetching events', error)
+    }
+  }
+
+  const addEvent = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:4000/api/vt/events/create', {
+        event: newEvent
+      })
+      console.log('Response data: ', response.data) // this will log the response data
+      setEvents([...events, response.data.events])
+      setNewEvent('')
+    } catch (error) {
+      console.error('Error adding event: ', error)
+      if (error.response && error.response.data && error.response.data.error) {
+        setError(error.response.data.error)
+      } else {
+        setError('Error adding event')
+      }
+    }
+  }
+
   return (
     <EventCalendarContainer style={{ paddingLeft: 250 }}>
       <Sidebar />
@@ -25,14 +62,17 @@ const EventCalendar = () => {
           {/* Here we can display calendar */}
           Calendar
         </CalendarContainer>
-        <AddEventForm>
+        <AddEventForm onSubmit={addEvent}>
           <h2>Add New Event</h2>
-          <EventInput type="text" placeholder="enter event" />
+          <EventInput type="text" value={newEvent} onChange={(e) => setNewEvent(e.target.value)} placeholder="enter event" />
           <AddEventButton type="submit">Add Event</AddEventButton>
         </AddEventForm>
 
         <Events>
           <h2>Events</h2>
+          {events.map((event, index) => {
+            <Event key={index}>{event}</Event>
+          })}
         </Events>
       </Content>
     </EventCalendarContainer>
