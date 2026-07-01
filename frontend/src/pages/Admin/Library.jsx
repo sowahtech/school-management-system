12615dccb2bf05f159fc504d42223b62d71782cc
Justice Sowah <sowahtech@gmail.com>
2026-07-01 21:11:02 +0000

    connected library.jsx to backend

--- frontend/src/pages/Admin/Library.jsx
+++ frontend/src/pages/Admin/Library.jsx
@@ -19,12 +19,57 @@ import {
 } from "../../styles/LibraryStyles";
 
 const Library = () => {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    fetchBooks();
+  }, [])
+
+  const fetchBooks = async () => {
+    try {
+      const response = await axios.get("http://localhost:4000/api/vt/library/getall")
+      setBooks(response.data.books)
+    } catch (error) {
+      console.error('Error fetching books', error)
+    }
+  }
+
+  const addBook = async (book) => {
+    try {
+      const response = await axios.post('http://localhost:4000/api/vt/library/create', {
+        bookname: book.title,
+        author: book.author
+      })
+      console.log('Response data: ', response.data) // this will log the response data
+      setBooks([...books, response.data])
+    } catch (error) {
+      console.error('Error adding book: ', error)
+    }
+  }
+
+  const handleBookPick = async (bookId, studentId) => {
+    // here, we can add logic when a student picks a book
+
+  }
+
+  const handleBookReturn = async (bookId, studentId) => {
+    // here, we can handle logic when a student returns a book
+  }
+
   return (
     <LibraryContainer style={{ paddingLeft: 250 }}>
       <Sidebar />
       <Content>
         <Title>Library Management</Title>
-        <AddBookForm>
+        <AddBookForm onSubmit={(e) => {
+          e.preventDefault()
+          const book = {
+            id: Math.random().toString(36).substring(2, 9),
+            title: e.target.title.value,
+            author: e.target.author.value,
+          }
+          addBook(book)
+        }}>
           <h2>Add New Book</h2>
           <FormGroup>
             <Label htmlFor="title">Title</Label>
@@ -37,7 +82,16 @@ const Library = () => {
           <Button type="submit">Add Book</Button>
         </AddBookForm>
         <h2>Books</h2>
-        <BookList></BookList>
+        <BookList>
+          {books.map((book) => {
+            <BookItem key={book._id}>
+              <BookTitle>{book.bookname}</BookTitle>
+              <BookAuthor>{book.author}</BookAuthor>
+              <ActionButton onClick={() => handleBookPick(book._id, student123)}>Pick</ActionButton>
+              <ActionButton onClick={() => handleBookReturn(book._id, student123)}>Return</ActionButton>
+            </BookItem>
+          })}
+        </BookList>
       </Content>
     </LibraryContainer>
   );
