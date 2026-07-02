f808027b734d994033807b89c99005c0fc1d85cf
Justice Sowah <sowahtech@gmail.com>
2026-07-02 22:43:53 +0000

    conneted students/announcements.jsx to backend

--- frontend/src/pages/Students/Assignments.jsx
+++ frontend/src/pages/Students/Assignments.jsx
@@ -12,7 +12,24 @@ import {
   AssignmentDescription,
   AssignmentDoneMessage,
 } from "../../styles/AssignmentsStyles";
+
 const StudentAssignments = () => {
+
+  const [assignments, setAssignments] = useState([])
+
+  useEffect(() => {
+    fetchAssignments();
+  }, [])
+
+  const fetchAssignments = async () => {
+    try {
+      const response = await axios.get("http://localhost:4000/api/vt/assignments/getall")
+      setAssignments(response.data.assignments)
+    } catch (error) {
+      console.error('Error fetching assignments', error)
+    }
+  }
+
   return (
     <AssignmentsContainer>
       <SidebarContainer>
@@ -20,6 +37,12 @@ const StudentAssignments = () => {
       </SidebarContainer>
       <Content>
         <h1>Assignments</h1>
+        {assignments.map((assignment, id) => {
+          <AssignmentCard>
+            <AssignmentTitle>{assignment.title}</AssignmentTitle>
+            <AssignmentDescription>{assignment.description}</AssignmentDescription>
+          </AssignmentCard>
+        })}
       </Content>
     </AssignmentsContainer>
   );
