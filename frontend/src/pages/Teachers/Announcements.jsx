bdee720535b63270a6d1dcf0a0a93f7ce811f575
Justice Sowah <sowahtech@gmail.com>
2026-07-05 23:54:40 +0000

    connected teachers/announcements.jsx to backend

--- frontend/src/pages/Teachers/Announcements.jsx
+++ frontend/src/pages/Teachers/Announcements.jsx
@@ -17,21 +17,58 @@ import {
 } from "../../styles/AnnouncementStyles";
 
 const CheckAnnouncementSection = () => {
+
+  const [announcement, setAnnouncement] = useState([])
+  const [announcements, setAnnouncements] = useState([])
+
+  useEffect(() => {
+    fetchAnnouncements();
+  }, [])
+
+  const fetchAnnouncements = async () => {
+    try {
+      const response = await axios.get("http://localhost:4000/api/vt/announcement/getall")
+      setAnnouncements(response.data.announcements)
+    } catch (error) {
+      console.error('Error fetching announcements', error)
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:4000/api/vt/announcement/create', {
+        announcement: announcement
+      })
+      console.log('Announcement sent: ', response.data) // this will log the response data
+      setAnnouncement('')
+      fetchAnnouncements()
+    } catch (error) {
+      console.error('Error sending announcement: ', error)
+    }
+  }
+
   return (
     <AnnouncementContainer>
       <Sidebar />
       <Content>
         <Title>Announcement</Title>
-        <AnnouncementForm>
+        <AnnouncementForm onSubmit={handleSubmit}>
           <FormGroup>
             <Label htmlFor="announcement">Announcement: </Label>
-            <TextArea id="announcement" required rows={4} cols={50} />
+            <TextArea id="announcement" value={announcement} onChange={(e) => setAnnouncement(e.target.announcement)} required rows={4} cols={50} />
           </FormGroup>
           <Button type="submit">Send Announcement</Button>
         </AnnouncementForm>
         {/* To display announcements */}
         <h2>Announcement</h2>
-        <AnnouncementList></AnnouncementList>
+        <AnnouncementList>
+          {announcements.map((announcement) => {
+            <AnnouncementItem key={announcement}>
+              <AnnouncementContent>{announcement.announcement}</AnnouncementContent>
+            </AnnouncementItem>
+          })}
+        </AnnouncementList>
       </Content>
     </AnnouncementContainer>
   );
