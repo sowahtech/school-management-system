2984c5f4e00e60c96f7b5d6e4fbe165728405987
Justice Sowah <sowahtech@gmail.com>
2026-07-05 23:40:08 +0000

    connected students/library.jsx to backend

--- frontend/src/pages/Students/Library.jsx
+++ frontend/src/pages/Students/Library.jsx
@@ -3,29 +3,58 @@ import Sidebar from "./Sidebar";
 import axios from "axios";
 
 import {
-  LibraryContainer,
-  SidebarContainer,
-  Content,
-  LibraryHeader,
-  BookList,
-  BookItem,
-  BookTitle,
-  BorrowButton,
+	LibraryContainer,
+	SidebarContainer,
+	Content,
+	LibraryHeader,
+	BookList,
+	BookItem,
+	BookTitle,
+	BorrowButton,
 } from "../../styles/LibraryStyles";
 
 const LibrarySection = () => {
-  return (
-    <LibraryContainer>
-      <SidebarContainer>
-        <Sidebar />
-      </SidebarContainer>
-      <Content>
-        <LibraryHeader>Library</LibraryHeader>
-        {/* here we will map through the books coming from the database */}
-        <BookList></BookList>
-      </Content>
-    </LibraryContainer>
-  );
+
+	const [books, setBooks] = useState([])
+
+	useEffect(() => {
+		fetchBooks();
+	}, [])
+
+	const fetchBooks = async () => {
+		try {
+			const response = await axios.get("http://localhost:4000/api/vt/library/getall")
+			setBooks(response.data.books)
+		} catch (error) {
+			console.error('Error fetching books', error)
+		}
+	}
+
+	const handleBorrowBook = (id) => {
+		// here, we can add logic when a student picks a book
+
+	}
+
+	return (
+		<LibraryContainer>
+			<SidebarContainer>
+				<Sidebar />
+			</SidebarContainer>
+			<Content>
+				<LibraryHeader>Library</LibraryHeader>
+				{/* here we will map through the books coming from the database */}
+				<BookList>
+					{books.map((book) => {
+						<BookItem key={book._id}>
+							<BookTitle>{book.bookname}</BookTitle>
+							<BookAuthor>{book.author}</BookAuthor>
+							<BorrowButton onClick={() => handleBorrowBook(book._id)}>Borrow</BorrowButton>
+						</BookItem>
+					})}
+				</BookList>
+			</Content>
+		</LibraryContainer>
+	);
 };
 
 export default LibrarySection;
